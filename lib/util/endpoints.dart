bf2adb99fd35f9b71859defa265145f3691a6365
ambartyagi <ambartyagi@gmail.com>
2020-02-07 19:33:54 +0530

    Added Email Sending Function

--- lib/util/endpoints.dart
+++ lib/util/endpoints.dart
@@ -15,5 +15,9 @@ class Endpoints {
     return 'http://ketchuprequestsfunctions-20200129102746440.azurewebsites.net/api/HttpTrigger-Java?code=HXlNyP13u1ffHaBWKta6m8AxiflCccjXug/h8s9tcXVkBei7QiOlRQ==&userId=$userId';
   };
 
+  static Function kSendCalendarInviteOnEventAcceptance = (dynamic startTime, dynamic endTime, dynamic eventName, dynamic eventDesc, dynamic recipientEmailId, dynamic organizerEmailId) {
+return 'http://ketchuprequestsfunctions-20200129102746440.azurewebsites.net/api/SendOutlookInvite-Java?code=BfZBJmpqY/IykovwkqJKRO6yfTkjvIxKFz6cay4ikDNbMSi4gqmwJQ==&startTime=$startTime&endTime=$endTime&eventName=$eventName&eventDesc=$eventDesc&recipientEmailId=$recipientEmailId&organizerEmailId=$organizerEmailId';
+  };
+
   static final kcreateEvent = '${Endpoints.host}events';
 }
